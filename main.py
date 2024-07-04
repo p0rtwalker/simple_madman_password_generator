f3ee09828cbfe0fc5d9aceb30f9eee1e3b7f0aeb
portwalker <150483877+portwalker@users.noreply.github.com>
2024-07-04 05:34:40 +0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 
 #this is a simple code in python to generate passwords 
 
-def generate_password(length=20): #<== here you can modify the length or the password to be generated
+def generate_password(length=23): #<== here you can modify the length or the password to be generated
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
