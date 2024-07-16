37141461b23b7003190c386d4f6f841c9f31a798
p0rtw4lk3r <150483877+p0rtwalker@users.noreply.github.com>
2024-07-16 16:47:31 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-#this is a simple code in python to generate passwords 
+#this is a simple code in python that generates passwords 
 
 def generate_password(length=23): #<== here you can modify the length or the password to be generated
     characters = string.ascii_letters + string.digits + string.punctuation
